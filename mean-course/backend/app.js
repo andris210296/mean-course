4cd3440ff6958d09ce6b7a10f87c0c1a3ecfcf43
Andris Andermann <andris.andermann@terra.com.br>
2019-10-11 14:59:50 -0300

    Remodeling data from DB to App data
    This commit allows to remodelling data that comes from MongoDB in to the structure that the application side is expecting.

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -41,23 +41,13 @@ app.post('/api/posts', (req, res, next) => {
 
 app.get('/api/posts', (req, res, next) => {
 
-  const posts = [
-    {
-      id:'6a85s7d6',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id:'6875sd1cf',
-      title: 'Second server-side post',
-      content: 'This is coming from the server'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched succesfully',
-    posts: posts
+  Post.find()
+    .then(documents=>{
+      res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: documents
+      });
+    });
   });
-});
 
 module.exports = app;
--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -11,12 +12,23 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+    .get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPostData => {
+        this.posts = transformedPostData;
         this.postsUpdated.next([...this.posts]);
       });
   }
+
   getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
