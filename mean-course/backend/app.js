d49e7a5adc83390e717c8ab953a714e5298aec70
Andris Andermann <andris.andermann@terra.com.br>
2019-10-11 09:45:27 -0300

    Update app.js
    This commit updates the file app.js usinf JSON as a response

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+  const posts = [
+    {
+      id:'6a85s7d6',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id:'6875sd1cf',
+      title: 'Second server-side post',
+      content: 'This is coming from the server'
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
