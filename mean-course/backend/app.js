9a620d7056f6ed629737a67bcdab11bd27f47a26
Andris Andermann <andris.andermann@terra.com.br>
2019-10-14 15:08:35 -0300

    Update Code
    This commit contains the code that updates Posts.

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -7,23 +7,23 @@ const Post = require('./models/post')
 const app = express();
 
 mongoose.connect("mongodb+srv://max:Ft6ktsjXLfr7Al5P@cluster0-icg8a.mongodb.net/mean-project?retryWrites=true&w=majority")
-.then(()=>{
-  console.log("Connected to database");
-})
-.catch(()=>{
-  console.log("Connection failed");
-});
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin","*");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-    );
-   res.setHeader("Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS");
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   next();
 });
 
@@ -41,22 +41,43 @@ app.post('/api/posts', (req, res, next) => {
   });
 });
 
-app.get('/api/posts', (req, res, next) => {
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
 
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Post updated successfully' });
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   Post.find()
-    .then(documents=>{
+    .then(documents => {
       res.status(200).json({
         message: 'Posts fetched succesfully',
         posts: documents
       });
     });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not Foud!' });
+    }
   });
+});
 
-
-app.delete('/api/posts/:id', (req, res, next) =>{
-  Post.deleteOne({_id: req.params.id}).then(result =>{
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
     console.log(result);
-    res.status(200).json({message: 'Post deletd"'});
+    res.status(200).json({ message: 'Post deletd"' });
   });
 });
 
