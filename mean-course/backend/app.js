e46cf4d9c6601dce900398e1981e237d72390246
Andris Andermann <andris.andermann@terra.com.br>
2019-10-18 10:57:45 -0300

    Listing Posts with images
    This commit saves the path on the database and presents the image on the post listing.

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -16,6 +17,7 @@ mongoose.connect("mongodb+srv://max:Ft6ktsjXLfr7Al5P@cluster0-icg8a.mongodb.net/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
