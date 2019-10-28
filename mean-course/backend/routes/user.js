a58fbd221a26a9bb875338dcb1ae2e3c9083ee85
Andris Andermann <andris.andermann@terra.com.br>
2019-10-28 14:54:05 -0300

    Login using token
    This commit changes the backend side and adds the login route with a token creation.

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
 const router = express.Router();
 
@@ -23,6 +25,36 @@ router.post("/signup", (req, res, next) => {
             error: err
           });
         });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password)
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign({ email: user.email, userId: user._id },
+        'secret_this_shoul_be_longer', { expiresIn: '1h' } );
+
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
 });
 
