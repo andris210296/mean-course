695a99b29e4028395c16c003fc238f6b3c1be3fc
Andris Andermann <andris.andermann@terra.com.br>
2019-11-08 11:04:34 -0300

    Update on Angular environment fields file

--- mean-course/src/environments/environment.prod.ts
+++ mean-course/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://meancourse-env.wtpannpbvc.us-east-2.elasticbeanstalk.com/api"
+  apiUrl: 'http://meancourseenv-env.2bvthtp4bp.us-east-2.elasticbeanstalk.com/api'
 };
